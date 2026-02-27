8253dd93aeecc5e4dddf986252c89a278a99d9d6
aelaraby <ahmedelaraby11.94@gmail.com>
2026-02-27 23:03:53 +0200

    Print Examples

---
+++ src/Print.kt
@@ -0,0 +1,28 @@
+
+// fun is used to declare a function .
+// The main() function is where your program starts from .
+    fun main() {
+
+    // println() and print() functions print their arguments to standard output
+
+
+
+
+            print("Hello, world!")
+     /* Prints text to the console.
+        Does NOT move to a new line after printing.
+        Any output written after it will appear on the same line. */
+
+
+       println("Hello, world!")
+
+     /* Prints text to the console.
+        Automatically moves to a new line after printing.
+        Any output written after it will start on a new line. */
+
+
+    }
+
+
+
+
